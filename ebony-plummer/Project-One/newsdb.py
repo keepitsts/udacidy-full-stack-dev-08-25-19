4aca1e855d2b65f0b31dbd38b7527f8d93d1893e
Ebony N. Plummer <ebony.plummer@gmail.com>
2019-07-11 22:33:02 -0400

    rewrote code that was overwritten with my old code due to a Github error.

--- ebony-plummer/Project-One/newsdb.py
+++ ebony-plummer/Project-One/newsdb.py
@@ -2,24 +2,37 @@ import psycopg2
 
 dbname = "news"
 
-db = psycopg2.connect("dbname=news")
 
-def top_three_articles():   
-    c = db.cursor()   
-    #top_articles =c.execute("select path,count(path) as hits from log group by path") 
-    top_articles = c.execute("select slug, count slug as pageviews from articles \
-    left join log on replace(log.path, '/article/', ') = articles.slug group by slug") 
+def top_three_articles():
+    db = psycopg2.connect(database=dbname)
+    c = db.cursor() 
+    """  
+    top_articles =c.execute("select path,count(path) as hits from log 
+    group by path")
+    """
+    top_articles = c.execute("select slug, count slug as pageviews from \
+    articles left join log on replace(log.path, '/article/', ') = \
+    articles.slug group by slug")
+    db.close()
     return top_articles
 
 
 def top_author() 
-c = db.cursor()   
-    #top_articles =c.execute("select path,count(path) as hits from log group by path") 
-    top_author = c.execute("select slug, count slug as pageviews from articles \
-    left join log on replace(log.path, '/article/', ') = articles.slug group by slug") 
+    db = psycopg2.connect(database=dbname)
+    c = db.cursor()
+    """  
+    top_articles =c.execute("select path,count(path) as hits from log group by path")
+    """ 
+    top_author = c.execute("select slug, count slug as pageviews from articles\
+    left join log on replace(log.path, '/article/', ') = articles.slug group\
+    by slug")
+    db.close()
     return top_author
 
 def errors()
-    db.cursor()
-    error_percentage = c.execute('select time, status, count(status) as num_errors group\
-     by time order by num_errors ')
+    db = psycopg2.connect(database=dbname)
+    c = db.cursor()
+    error_percentage = c.execute('select time, status, count(status) as num_errors\
+    group by time order by num_errors ')
+    db.close()
+    return errors
